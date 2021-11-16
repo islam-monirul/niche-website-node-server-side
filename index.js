1142aead59b9b0412e7bed145a76367be7abefcd
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-11-16 08:36:01 +0600

    database connection successfull

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -8,6 +9,29 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9tg9f.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database connection successful!");
+
+    const database = client.db("motoMaze");
+    const bikeCollection = database.collection("bikes");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Server Side!");
